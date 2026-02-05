a41de13438498f52ce793bdb0f25aebb08ad1746
Valentin Romanov <22451680+valrom@users.noreply.github.com>
2026-02-05 22:18:55 +0300

    First realization

---
+++ src/First/First.cs
@@ -0,0 +1,65 @@
+﻿#define TEST_FOR_NON_LATIN
+
+using System.Security;
+using System.Text.RegularExpressions;
+
+namespace First;
+
+public class LatinCompress
+{
+
+#if TEST_FOR_NON_LATIN
+    private static Regex ContainOnlyLatin() => new Regex(@"^[a-zA-Z]+$");
+#endif
+
+    public static string Comporess(string input)
+    {
+#if TEST_FOR_NON_LATIN
+
+        if (!ContainOnlyLatin().IsMatch(input))
+        {
+            throw new ArgumentException("Input must be only latin characters");
+        }
+
+#endif
+
+        if (input == "" || input.Length == 1)
+        {
+            return input;
+        }
+
+        string output = "";
+        int count = 1;
+        char current = input[0];
+
+        var flushToOutput = () =>
+        {
+            string newPart = current.ToString();
+
+            if (count > 1)
+            {
+                newPart += count.ToString();
+            }
+
+            output += newPart;
+        };
+
+        for (var i = 0; i < input.Length; ++i)
+        {
+            if (input[i] == current)
+            {
+                count += 1;
+            }
+            else
+            {
+                flushToOutput();
+                current = input[i];
+                count = 1;
+            }
+        }
+
+        flushToOutput();
+
+        return output;
+    }
+}
